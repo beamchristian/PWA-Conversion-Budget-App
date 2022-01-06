32aa17b4c2a05bcec71fd412c36bc23a13c1a39a
beamchristian <beamchristian@yahoo.com>
2022-01-06 18:06:00 -0500

    saveRecord function

--- public/js/idb.js
+++ public/js/idb.js
@@ -16,10 +16,10 @@ request.onsuccess = function (event) {
   // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
   db = event.target.result;
 
-  // check if app is online, if yes run createTransaction() function to send all local db data to api
+  // check if app is online, if yes run uploadTransaction() function to send all local db data to api
   if (navigator.onLine) {
     // we haven't created this yet, but we will soon, so let's comment it out for now
-    // createTransaction();
+    // uploadTransaction();
   }
 };
 
@@ -27,3 +27,28 @@ request.onerror = function (event) {
   // log error here
   console.log(event.target.errorCode);
 };
+
+// This function will be executed if we attempt to submit a new transaction and there's no internet connection
+function saveRecord(record) {
+  // open a new transaction with the database with read and write permissions
+  const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+  // access the object store for `new_transaction`
+  const transactionObjectStore = transaction.objectStore('new_transaction');
+
+  // add record to your store with add method
+  transactionObjectStore.add(record);
+}
+
+function uploadTransaction() {
+  // open a transaction on your db
+  const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+  // access your object store
+  const transactionObjectStore = transaction.objectStore('new_transaction');
+
+  // get all records from store and set to a variable
+  const getAll = transactionObjectStore.getAll();
+
+  // more to come...
+}
